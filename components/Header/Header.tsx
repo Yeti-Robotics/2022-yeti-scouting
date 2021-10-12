ab49644c04df7533b3d66252a0bb270bab43c7d7
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-10-12 17:48:20 -0400

    it will build now

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import React, { useCallback, useContext, useEffect } from 'react';
 import { HeaderWrapper, LogoWrapper } from './HeaderStyles';
-import Dropdown from '../Dropdown';
 import ClickableDropdown from '../ClickableDropdown';
 import { useHidingHeader } from '../../hooks/useHidingHeader';
 import Icicles from '../Icicles';
-import { StaticImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby';
+import Image from 'next/image';
+import Link from 'next/link';
 import { HeaderContext } from './Context';
 import { GlobalContext } from '@/GlobalContext';
 
@@ -42,117 +41,83 @@ const Header: React.FC = () => {
 		<>
 			<HeaderWrapper ref={headerRef}>
 				<LogoWrapper>
-					<Link to='/'>
-						<StaticImage
+					<Link href='/'>
+						<Image
 							src='../../images/yeti-logo-with-text2.png'
 							height={82}
 							width={150}
 							loading='eager'
-							placeholder='blurred'
 							alt='YETI Logo'
 						/>
 					</Link>
 				</LogoWrapper>
 
 				{/* Changes header based on window's width */}
-				{windowDimensions.width >= 1250 ? (
-					<>
-						<Dropdown title='About' href='/about' items={[]} />
-						<Dropdown title='Join Us' href='/join-us' items={[]} />
-						<Dropdown title='Outreach' href='/outreach' items={[]} />
-						<Dropdown
-							title='Robots'
-							href='/robots'
-							items={[
-								{ title: 'Polaris 2019', href: '/robots/2019' },
-								{ title: 'Avalanche 2018', href: '/robots/2018' },
-								{ title: 'Permafrost 2017', href: '/robots/2017' },
-								{ title: 'Black Ice 2016', href: '/robots/2016' },
-								{ title: 'Frostbyte 2015', href: '/robots/2015' },
-								{ title: 'Fluffy 2014', href: '/robots/2014' },
-								{ title: 'Momo 2013', href: '/robots/2013' },
-								{ title: 'Chompa 2012', href: '/robots/2012' },
-								{ title: 'Wampa 2011', href: '/robots/2011' },
-							]}
-						/>
-						<Dropdown
-							title='Mentors'
-							href='/mentors'
-							items={[
-								{ title: 'Technical Mentors', href: '/mentors/technical-mentors' },
-								{ title: 'Business Mentors', href: '/mentors/business-mentors' },
-							]}
-						/>
-						<Dropdown title='Sponsors' href='/sponsors' items={[]} />
-						<Dropdown title='Contact' href='/contact' items={[]} />
-					</>
-				) : windowDimensions.width > 0 ? (
-					<>
-						<ClickableDropdown
-							items={[
-								{
-									title: 'About',
-									href: '/about',
-									children: [],
-								},
-								{
-									title: 'Join Us',
-									href: '/join-us',
-									children: [],
-								},
-								{
-									title: 'Outreach',
-									href: '/outreach',
-									children: [],
-								},
-								{
-									title: 'Robots',
-									href: '/robots',
-									children: [
-										{ title: 'Polaris 2019', href: '/robots/2019' },
-										{ title: 'Avalanche 2018', href: '/robots/2018' },
-										{ title: 'Permafrost 2017', href: '/robots/2017' },
-										{ title: 'Black Ice 2016', href: '/robots/2016' },
-										{ title: 'Frostbyte 2015', href: '/robots/2015' },
-										{ title: 'Fluffy 2014', href: '/robots/2014' },
-										{ title: 'Momo 2013', href: '/robots/2013' },
-										{ title: 'Chompa 2012', href: '/robots/2012' },
-										{ title: 'Wampa 2011', href: '/robots/2011' },
-									],
-								},
-								{
-									title: 'Mentors',
-									href: '/mentors',
-									children: [
-										{
-											title: 'Technical Mentors',
-											href: '/mentors/tech-mentors',
-										},
-										{
-											title: 'Business Mentors',
-											href: '/mentors/business-mentors',
-										},
-									],
-								},
-								{
-									title: 'Sponsors',
-									href: '/sponsors',
-									children: [],
-								},
-								{
-									title: 'Contact',
-									href: '/contact',
-									children: [],
-								},
-								{
-									title: 'Donate',
-									href: 'https://paypal.com',
-									children: [],
-								},
-							]}
-						/>
-					</>
-				) : null}
+				<>
+					<ClickableDropdown
+						items={[
+							{
+								title: 'About',
+								href: '/about',
+								children: [],
+							},
+							{
+								title: 'Join Us',
+								href: '/join-us',
+								children: [],
+							},
+							{
+								title: 'Outreach',
+								href: '/outreach',
+								children: [],
+							},
+							{
+								title: 'Robots',
+								href: '/robots',
+								children: [
+									{ title: 'Polaris 2019', href: '/robots/2019' },
+									{ title: 'Avalanche 2018', href: '/robots/2018' },
+									{ title: 'Permafrost 2017', href: '/robots/2017' },
+									{ title: 'Black Ice 2016', href: '/robots/2016' },
+									{ title: 'Frostbyte 2015', href: '/robots/2015' },
+									{ title: 'Fluffy 2014', href: '/robots/2014' },
+									{ title: 'Momo 2013', href: '/robots/2013' },
+									{ title: 'Chompa 2012', href: '/robots/2012' },
+									{ title: 'Wampa 2011', href: '/robots/2011' },
+								],
+							},
+							{
+								title: 'Mentors',
+								href: '/mentors',
+								children: [
+									{
+										title: 'Technical Mentors',
+										href: '/mentors/tech-mentors',
+									},
+									{
+										title: 'Business Mentors',
+										href: '/mentors/business-mentors',
+									},
+								],
+							},
+							{
+								title: 'Sponsors',
+								href: '/sponsors',
+								children: [],
+							},
+							{
+								title: 'Contact',
+								href: '/contact',
+								children: [],
+							},
+							{
+								title: 'Donate',
+								href: 'https://paypal.com',
+								children: [],
+							},
+						]}
+					/>
+				</>
 				<Icicles
 					parentRef={headerRef}
 					widthConstrains={{ max: 20, min: 15 }}
