82b119a051f3f1905e7b9a967f05349722cf0f2f
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-10-15 08:10:31 -0400

    made scoreInput js cause ts broke

--- components/ScoutingForm/ScoreInput.js
+++ components/ScoutingForm/ScoreInput.js
@@ -1,24 +1,10 @@
-import { Form } from '@/models/form';
 import { Control, Controller, FieldPath } from 'react-hook-form';
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { ScoreInputStyles } from './ScoutingFormStyles';
 
-interface ScoreInputProps {
-    label: string;
-    name: FieldPath<Form>;
-    control: Control<Form>;
-    defaultValue?: number;
-}
+/* Typescript was broken in here :pensive: */
 
-const ScoreInput: React.FC<ScoreInputProps> = ({ label, name, defaultValue, control, ...inputProps }) => {
-
-    const validateChange = (input: number, type: number) => {
-        if (input - 1 < 0) return input;
-        if (type > 0) return input + 1;
-        if (type < 0) return input -1;
-        if (type === 0) throw new Error('type argument to validateChange must be above or below 0');
-    };
-
+const ScoreInput = ({ label, name, control, ...inputProps }) => {
     return (
         <Controller
             name={name}
@@ -30,7 +16,7 @@ const ScoreInput: React.FC<ScoreInputProps> = ({ label, name, defaultValue, cont
             <label>{label}</label>
             <ScoreInputStyles>
                 <button style={{ left: -40, borderRadius: '3px 0 0 3px' }} type='button' disabled={value < 1} onClick={() => onChange(value - 1)}>-</button>
-                <input type='number' name={name} value={value} ref={ref} disabled />
+                <input type='number' name={name} value={value} ref={ref} disabled {...inputProps} />
                 <button style={{ right: -40 }} type='button' onClick={() => onChange(value + 1)}>+</button> 
             </ScoreInputStyles>
             </>
